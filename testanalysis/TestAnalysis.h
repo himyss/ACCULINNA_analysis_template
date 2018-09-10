f661222e6f180c2c688560f89520dffacd342757
himyss <ivanmuzalevskij@gmail.com>
2018-09-11 01:46:20 +0300

    reading inputs added

--- testanalysis/TestAnalysis.h
+++ testanalysis/TestAnalysis.h
@@ -38,6 +38,21 @@ private:
 	const TClonesArray* ftSQ300_clar;
 	const TClonesArray* fSQ300_clar;
 
+	Float_t fparXL1[32];
+	Float_t fparXL2[32];
+	Float_t fparYL1[16];
+	Float_t fparYL2[16];
+	Float_t fparXR1[32];
+	Float_t fparXR2[32];
+	Float_t fparYR1[16];
+	Float_t fparYR2[16];
+	Float_t fpar201[16];
+	Float_t fpar202[16];
+	Float_t fparCsR1[16];
+	Float_t fparCsR2[16];
+	Float_t ftAngle = 12.;
+ 
+
 
 	ClassDef(TestAnalysis, 1);
 };
